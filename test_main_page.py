6d69a90950170aa3e54a725d037c611042170044
Dusty Helmet <andromedk@gmail.com>
2021-10-16 19:18:24 +0500

    create test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,13 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+    page_login = LoginPage(browser, browser.current_url)
+    page_login.open()
+    page_login.should_be_login_page()
