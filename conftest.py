0485c9b9656d87db3246d8630195937ee86e3ecd
Dusty Helmet <andromedk@gmail.com>
2021-10-16 19:05:11 +0500

    some changes

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ import time
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="None",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose a language: en, ru ... etc!")
 
 @pytest.fixture(scope="function")
