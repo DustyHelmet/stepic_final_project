912a0645acc8249168a41f32e56cbbb86cb0a096
Dusty Helmet <andromedk@gmail.com>
2021-10-21 14:00:13 +0500

    add tests: -test_guest_cant_see_success_message_after_adding_product_to_basket -test_guest_cant_see_success_message -test_message_disappeared_after_adding_product_to_basket -test_guest_should_see_login_link_on_product_page -test_guest_can_go_to_login_page_from_product_page
    Signed-off-by: Dusty Helmet <andromedk@gmail.com>

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,52 @@
+from .pages.locators import ProductPageLocators
 from .pages.product_page import PageObject
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+num_promo = [pytest.param(num, marks=pytest.mark.xfail) if num == 7 else num for num in range(10)]
+
+@pytest.mark.parametrize('num', num_promo)
+@pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer{num}"
+    page = PageObject(browser, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_a_message()
+    page.is_product_in_basket()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = PageObject(browser, link)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = PageObject(browser, link)
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = PageObject(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.go_to_login_page()
+
